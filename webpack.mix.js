937d07e14c51603d4a99ca5b30ab400c968c1a43
Vladyslav Bondarenko <vladbndko@gmail.com>
2022-07-20 19:12:00 +0300

    Disable mix-manifest, add asset folder

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,15 +2,16 @@ const mix = require('laravel-mix');
 require('laravel-mix-nunjucks');
 
 mix
-  .njk('./src/views', 'public/', {
+  .njk('./src/views', 'dist/', {
     manageEnv(nunjucks) {
       nunjucks.addFilter('version', (filename) => {
         return `${filename}?hash=${Date.now()}`;
       });
     },
   })
-  .sass('src/styles/app.scss', '/')
-  .js('src/scripts/app.js', '/')
+  .copyDirectory('src/assets', 'dist')
+  .sass('./src/styles/app.scss', '/')
+  .js('./src/scripts/app.js', '/')
   .options({
     processCssUrls: false,
     sassOptions: {
@@ -20,17 +21,19 @@ mix
     terser: {
       extractComments: false,
     },
+    manifest: false,
   })
-  .setPublicPath('public');
+  .setPublicPath('dist');
 
 if (!mix.inProduction()) {
   mix
     .browserSync({
       notify: false,
-      server: 'public',
+      server: 'dist',
       proxy: null,
       open: false,
       watch: true,
+      files: ['./src/**/*'],
     })
     .disableSuccessNotifications()
     .sourceMaps()
