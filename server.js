323680a75a9c7d37c17fd39f006b871ac5002940
Vladyslav Bondarenko <vladbndko@gmail.com>
2022-08-10 13:24:12 +0300

    Add port const

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const static = require('node-static');
 const http = require('http');
 
+const PORT = 8080;
+
 const file = new static.Server(`${__dirname}/dist`);
 
 http
   .createServer((req, res) => {
     file.serve(req, res);
   })
-  .listen(8080);
+  .listen(PORT);
